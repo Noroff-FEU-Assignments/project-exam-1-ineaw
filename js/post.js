981c2e7a6ce833321bd7d3db3f0145f89b5ff317
ineaw <ineaw@users.noreply.github.com>
2021-05-23 16:40:15 +0200

    removed care info part

--- js/post.js
+++ js/post.js
@@ -57,3 +57,32 @@ async function getPosts() {
 }
 
 getPosts();
+
+const openImage = document.querySelectorAll(".modal-button");
+// const closeImage = document.querySelectorAll("[data-close]");
+const visible = "is-visible";
+
+/* Iterate over object and add modal */
+
+for (const img of openImage) {
+  img.addEventListener("click", function () {
+    const modal = this.dataset.open;
+    document.getElementById(modal).classList.add(visible);
+  });
+}
+
+/* Close by clicking outside image */
+
+document.addEventListener("click", (e) => {
+  if (e.target === document.querySelector(".post-modal.is-visible")) {
+    document.querySelector(".post-modal.is-visible").classList.remove(visible);
+  }
+});
+
+/* Close with escape button */
+
+document.addEventListener("keyup", (e) => {
+  if (e.key === "Escape" && document.querySelector(".post-modal.is-visible")) {
+    document.querySelector(".post-modal.is-visible").classList.remove(visible);
+  }
+});
